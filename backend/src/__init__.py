9401e2619edcc6cc58ed3b59ef44291a6a1c175c
kk10 <kamy.zero.10@gmail.com>
2025-11-29 11:48:36 +0300

    feat(backend): add database.py with engine, Base, SessionLocal and get_db dependency

---
+++ backend/src/__init__.py
@@ -0,0 +1,6 @@
+from .database import Base, get_db
+
+__all__ = [
+    "Base",
+    "get_db"
+]
---
+++ backend/src/database.py
@@ -0,0 +1,31 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
